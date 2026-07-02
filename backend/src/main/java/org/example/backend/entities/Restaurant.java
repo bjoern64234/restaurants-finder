950853ef94c8793c1fb495e10fe22272e765214f
bjoern <b.irion@wtnet.de>
2026-07-02 17:19:28 +0200

    Add restaurant table

---
+++ backend/src/main/java/org/example/backend/entities/Restaurant.java
@@ -0,0 +1,45 @@
+package org.example.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor //JPA required this
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String formatted;
+
+    private String website;
+
+    private String openingHours;
+
+    private String cuisine;
+
+    private Double lat;
+
+    private Double lng;
+
+    private String placeId;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
