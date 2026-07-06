77ae3718b19efb1dfb832b2f11c40d1b61763e57
Ilham Muhammad <ilhamuh97@gmail.com>
2026-07-06 16:14:27 +0200

    fix build error

--- frontend/src/components/AuthDialog/AuthDialog.tsx
+++ frontend/src/components/AuthDialog/AuthDialog.tsx
@@ -76,7 +76,7 @@ export function AuthDialog({open, onClose}: Readonly<Props>): ReactElement {
         if (event.target === dialogRef.current) onClose();
     }
 
-    const handleDialogKeyDown = (e) => {
+    const handleDialogKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
         if (e.key === "Escape") {
             onClose();
         }
